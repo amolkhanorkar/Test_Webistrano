d3c9c3143497d1fb1453c9e87bfcf88fde726dcd
Amol <webonise@amol-desktop.(none)>
2013-01-07 15:22:50 +0530

    initial commit from develop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestWebistrano::Application.config.secret_token = 'e32ac188f009c8967a6bc39a95e5544734bc7994bd9db86a2c5099a79c132ccad0bb7ba89ef3a9030d6c0ce20bdf02c5286dfa5275c6c4f3fc93d2495aa10eb0'
